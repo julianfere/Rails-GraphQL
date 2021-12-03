b5045a552ff08906da4a3a5b7a853057d5b3b531
Julian Feregotti <julian.feregotti@snappler.com>
2021-12-03 18:12:07 -0300

    Added first and last params to user query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,19 +1,28 @@
 module Types
   class QueryType < Types::BaseObject
     # /users
-
-    field :users, [Types::UserType], null: false
-
-    def users
-      User.all
+    field :users, [Types::UserType], null: false do
+      argument :last,  Integer, required: false
+      argument :first, Integer, required: false
     end
 
+    def users(**query)
+      if query[:first]
+        User.first(query[:first])
+      elsif query[:last]
+        User.last(query[:last])
+      end
+    end
+
+    # /user/:id
     field :user, Types::UserType, null: false do
       argument :id, ID, required: true
     end
 
     def user(id:)
       User.find(id)
+    rescue ActiveRecord::RecordNotFound
+      User.new
     end
   end
 end
